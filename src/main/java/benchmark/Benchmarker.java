fab74f0c2b69b768f76e2ff0a114aba03e482a3f
Valentin Bauer <e1425252@student.tuwien.ac.at>
2017-06-29 10:33:11 +0200

    added benchmarks

---
+++ src/main/java/benchmark/Benchmarker.java
@@ -0,0 +1,48 @@
+package benchmark;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import parser.binary.BinaryParser;
+
+/**
+ * Created by Valentin
+ * TODO documentation
+ */
+public class Benchmarker {
+    private static int BENCHMARK_ROUNDS_FACTORIAL = 10000;
+    private static int BENCHMARK_ROUNDS_FIBONACCI = 50;
+
+    public static void main(String[] args) {
+
+        long startTime = System.nanoTime();
+        try {
+            File f = new File("factorial.wasm");
+            int[] arguments = new int[0];
+            for (int i = 0; i < BENCHMARK_ROUNDS_FACTORIAL; i++) {
+                BinaryParser factorialParser = new BinaryParser();
+                factorialParser.parse(f, arguments);
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        double factSeconds = ((double)(System.nanoTime() - startTime) / 1000000000);
+
+        startTime = System.nanoTime();
+        try {
+            File f = new File("fibonacci.wasm");
+            int[] arguments = new int[0];
+            for (int i = 0; i < BENCHMARK_ROUNDS_FIBONACCI; i++) {
+                BinaryParser fibonacciParser = new BinaryParser();
+                fibonacciParser.parse(f, arguments);
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        double fibSeconds = ((double)(System.nanoTime() - startTime) / 1000000000);
+        System.out.println(BENCHMARK_ROUNDS_FACTORIAL + " rounds of 'factorial.wasm' ( fac(20) ) took "
+            + new DecimalFormat("#.##########").format(factSeconds) + " Seconds");
+        System.out.println(BENCHMARK_ROUNDS_FIBONACCI + " rounds of 'fibonacci.wasm' ( fib(30) ) took "
+            + new DecimalFormat("#.##########").format(fibSeconds) + " Seconds");
+    }
+}
